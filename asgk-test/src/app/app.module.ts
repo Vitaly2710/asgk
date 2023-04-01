58800555108d3fd01892321e2a47c65de51f20e2
Vitaly Cheastyakov <vchistiakov@datamola.com>
2023-04-01 17:12:41 +0300

    add initial components for login page

--- asgk-test/src/app/app.module.ts
+++ asgk-test/src/app/app.module.ts
@@ -2,15 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {ModalLoginComponent} from "./shared/components/login-modal/modal.login.component";
+import {LoginComponent} from "./pages/login/login.component";
+import { LoginFormComponent } from './shared/components/login-form/login-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModalLoginComponent,
+    LoginComponent,
+    LoginFormComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
+  exports: [
+    ModalLoginComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
